ceabdfb127e2eec888e42e4bc28e617802dbc251
DesaNtimo <davidvardanan700@gmail.com>
2025-03-04 18:14:22 +0400

    First commit.......

--- src/Main.java
+++ src/Main.java
@@ -30,12 +30,12 @@ class Main {
     }
 
     static void printMenu() {
-        System.out.println("========== Меню ==========");
+        System.out.println("====== Меню ======");
         System.out.println("1. Ввести количество шагов за определённый день");
         System.out.println("2. Изменить цель по количеству шагов в день");
         System.out.println("3. Напечатать статистику за определённый месяц");
         System.out.println("4. Выйти из приложения");
-        System.out.println("==========================");
+        System.out.println("===================");
         System.out.print("--> ");
     }
 }
